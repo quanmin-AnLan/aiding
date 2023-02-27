166b51ca990a6babce0a38d72e315947902dc89a
wnw <ningweiwang218830@sohu-inc.com>
2023-02-27 10:32:06 +0800

    init

--- src/main.js
+++ src/main.js
@@ -4,10 +4,19 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入全局样式
+import './less/main.less'
+import logonComponents from './components/config'
+import { fn } from '@/utils/common'
 
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+// 注册全局组件
+logonComponents(Vue)
+
+Vue.prototype.$fn = fn
 
 new Vue({
   router,
